52e6ad0805588c5f036887dfb3468c56ce15dc3f
bscottmay <bscottmay@gmail.com>
2020-11-27 10:25:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ setup(
     version='0.1.0',
     description='Sample package for Python-Guide.org',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Scott May',
+    author_email='bscottmay@gmail.com',
+    url='https://github.com/bscottmay/samplemod',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
